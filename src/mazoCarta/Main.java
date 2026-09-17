891f565fea6b386f44f1923e8984c5bcfe751786
Leonardo Dávalos <davaloslm@hotmail.com.ar>
2026-09-17 20:15:25 -0300

    Add: Clases Mazo y Carta

--- src/mazoCarta/Main.java
+++ src/mazoCarta/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Carta c1 = new Carta(0, 1);
-		c1.mostrar();
+//		Carta c1 = new Carta(0, 1);
+//		c1.mostrar();
 		
 		Mazo m1 = new Mazo();
-		m1.mostrar();
+//		m1.mostrar();
 		m1.mezclarMazo();
 		m1.mostrar();
 	}
