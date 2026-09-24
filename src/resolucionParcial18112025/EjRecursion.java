610d03fab6629ea3e37d5fa43c979c0356f94f06
Leonardo Dávalos <davaloslm@hotmail.com.ar>
2026-09-24 20:03:21 -0300

    Add: Ejercicios de recursion de parcial

--- src/resolucionParcial18112025/EjRecursion.java
+++ src/resolucionParcial18112025/EjRecursion.java
@@ -3,8 +3,29 @@ package resolucionParcial18112025;
 public class EjRecursion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println(colapsarRepetidos("aaabbcdd"));
+		System.out.println(colapsarRepetidos("holaaa"));
+		System.out.println(colapsarRepetidos("abc"));
+		System.out.println(colapsarRepetidos(""));
 	}
+	
+	public static String colapsarRepetidos(String s) {
+		if (s.length() <= 1) { // caso base
+			return s;
+		}
+		
+		//caso recursivo
+		if (s.charAt(0) == s.charAt(1)) {
+			return colapsarRepetidos(s.substring(1));
+		} else {
+			return s.charAt(0) + colapsarRepetidos(s.substring(1));
+		}
+		
+		
+	}
+	
+	
+	
 
 }
