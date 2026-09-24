8a60694ccc2459c56417e14476eec35cf56e4d68
Leonardo Dávalos <davaloslm@hotmail.com.ar>
2026-09-24 19:33:19 -0300

    Add: Ejercicios de objetos de parcial 18 11 2025

---
+++ src/resolucionParcial18112025/EjRecursion.java
@@ -0,0 +1,10 @@
+package resolucionParcial18112025;
+
+public class EjRecursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/resolucionParcial18112025/RedAlquiler.java
+++ src/resolucionParcial18112025/RedAlquiler.java
@@ -4,5 +4,46 @@ package resolucionParcial18112025;
 public class RedAlquiler {
 	Sucursal[] sucursales;
 	
+	public double gastoTotal(Cliente cliente, Ciudad ciudad) {
+		
+		double total = 0; //acumulador 
+		
+		//accedo a sucursales
+		for (Sucursal sucursal : sucursales) {
+			if (sucursal.ciudad.nombre.equals(ciudad.nombre)) {
+				for (Auto auto : sucursal.autos) {
+					for (Alquiler alquiler : auto.alquileres) {
+						if (alquiler.cliente.equals(cliente)) {
+							total += alquiler.costoTotal;
+						}
+					}
+				}
+			}
+		}
+		return total;
+	}
+	
+	//Ejercicio de recuperatorio de este parcial:
+	//Escribir el método public Cliente conAlquilerMasAlto() que retorna el cliente que realizó el alquiler ás costoso
+	
+	public Cliente conAlquilerMasAlto() {
+		Cliente clienteMax = null;
+		double alquilerMax = 0;
+		
+		for (Sucursal sucursal : sucursales) {
+			for (Auto auto : sucursal.autos) {
+				for (Alquiler alquiler : auto.alquileres) {
+					if (alquiler.costoTotal > alquilerMax) {
+						alquilerMax = alquiler.costoTotal;
+						clienteMax = alquiler.cliente;
+					}
+				}
+			}
+		}
+		return clienteMax;
+		
+		
+		
+	}
 	
 }
